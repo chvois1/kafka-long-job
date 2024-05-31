707ad068492cad06450b2968a710afe93654a89c
chvois1 <chr.voisin@orange.fr>
2024-05-31 10:52:34 +0200

    projetc init

---
+++ not-async/src/main/java/not/async/Application.java
@@ -0,0 +1,20 @@
+package not.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ * <br>
+ * <code>not.async.Application</code>
+ * <br>
+ *
+ * @author Xavyr Rademaker
+ * @since 08 March 2021
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
